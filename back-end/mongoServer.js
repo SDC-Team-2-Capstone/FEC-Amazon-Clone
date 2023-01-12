79cc1decb614a8af1c9c7af2b90ee88a6ee02401
Lordprettyflacko <snsolomar@gmail.com>
2023-01-12 13:15:08 -0600

    third commit

--- back-end/mongoServer.js
+++ back-end/mongoServer.js
@@ -14,37 +14,42 @@ const port = process.env.PORT || 8081;
 const client = new MongoClient({ connectionString: config.connectionString });
 client.connect;
 
+const mongoose = require("mongoose");
+mongoose.connect(MONGO_URI, { useNewUrlParser: true });
+
+/* ==================== Create Model for Data ==================== */
+
+const MyModel = mongoose.model("MyModel", { name: String });
+
 /* ==================== Middleware ==================== */
 
 app.use(cors());
 app.use(express.json());
 
-/* ========== General ========== */
-app.get("/", (req, res) => {
-  res.send("hello world");
+//Define the schema
+const MessageSchema = newmongoose.Schemua({
+  message: toString,
 });
 
-app.get("/recommendations", (req, res) => {
-  MongoClient.find().then((result) => {
-    res.send(result.rows);
+//Create a model from the schema
+const Message = mongoose.model("Message", MessageSchema);
+
+/* ========== General ========== */
+
+//Define 'hello world' route
+app.get("/helloworld", (req, res) => {
+  Message.findOne({ message: "Hello World" }, (err, message) => {
+    if (err) return res.status(500).send(err);
+    // Return the message as a response
+    res.send(message);
   });
 });
 
-// app.get("/recommmendations", function (req, res) {
-//   let response = res;
-
-//   client.connect(
-//     "mongodb://mongodb:docker@localhost:27017",
-//     function (err, client) {
-//       if (err) throw err;
-
-//       db = client.db().findOne(query, function (err, result) {
-//         if (err) throw err;
-//         client.close();
-//         response.send(result);
-//       });
-//     }
-//   );
+// app.get('/', (req, res) => {
+//   MyModel.find((err, data) => {
+//       if (err) return res.json({ success: false, error: err });
+//       return res.json({ success: true, data: data });
+//   });
 // });
 
 /* ==================== Listener ==================== */
