be1545be83352bf912165c526e07896bc004dcf1
Lordprettyflacko <snsolomar@gmail.com>
2023-01-11 22:11:03 -0600

    second commit

--- back-end/mongoServer.js
+++ back-end/mongoServer.js
@@ -1,20 +1,54 @@
+/* ==================== Dependencies ==================== */
+
 const express = require("express");
+const config = require("./config.js");
 const cors = require("cors");
+const { MongoClient } = require("mongodb");
 
-const MongoClient = require("mongodb").MongoClient;
-app.get("/get-profile", function (req, res) {
-  let response = res;
+/* ==================== Initialize Express ==================== */
+const app = express();
+const port = process.env.PORT || 8081;
 
-  MongoClient.connect(
-    "mongodb://mongodb:docker@localhost:27017",
-    function (err, client) {
-      if (err) throw err;
+/* ==================== Initialize Client ==================== */
 
-      db = client.db("users").findOne(query, function (err, result) {
-        if (err) throw err;
-        client.close();
-        response.send(result);
-      });
-    }
-  );
+const client = new MongoClient({ connectionString: config.connectionString });
+client.connect;
+
+/* ==================== Middleware ==================== */
+
+app.use(cors());
+app.use(express.json());
+
+/* ========== General ========== */
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.get("/recommendations", (req, res) => {
+  MongoClient.find().then((result) => {
+    res.send(result.rows);
+  });
+});
+
+// app.get("/recommmendations", function (req, res) {
+//   let response = res;
+
+//   client.connect(
+//     "mongodb://mongodb:docker@localhost:27017",
+//     function (err, client) {
+//       if (err) throw err;
+
+//       db = client.db().findOne(query, function (err, result) {
+//         if (err) throw err;
+//         client.close();
+//         response.send(result);
+//       });
+//     }
+//   );
+// });
+
+/* ==================== Listener ==================== */
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
